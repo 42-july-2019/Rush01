79954d0f2846a1cd524d901ae53abc69e10538ca
Nathanael Labreuil <alabreui@student.42.fr>
2019-07-14 17:59:03 +0200

    Added print array & Fixed argv utilisation

--- ex00/main.c
+++ ex00/main.c
@@ -6,52 +6,60 @@
 /*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/13 15:15:03 by alabreui          #+#    #+#             */
-/*   Updated: 2019/07/14 17:22:30 by alabreui         ###   ########.fr       */
+/*   Updated: 2019/07/14 17:57:33 by alabreui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 #include "utils.h"
 
-int	main(int argc, char **argv)
+void	ft_putchar(char c)
 {
-	char *str;
-	if (argc < 2)
-	{
-		//write(1, "Error\n", 7);
-		str = "4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2";
+	write(1, &c, 1);
+}
 
-	}
-	else
-	{		
-		str = argv[1];
-		// insert rest of main here...
-	}
-	int **array;
+void print_array(int *tab[SIZE + 2])
+{
 	int i;
 	int j;
 
-	array = generate_array(str);
-	if (!params_are_valid(str))
-		write(1, "Error\n", 7);
-	else
+	i = 1;
+	while (i <= SIZE)
 	{
-		if (!array_is_valid(array))
+		j = 1;
+		while (j <= SIZE)
+		{
+			ft_putchar(tab[i][j] + '0');
+			ft_putchar(' ');
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		write(1, "Error\n", 7);
+	}
+	else
+	{		
+		char *str;
+		int **array;
+
+		str = argv[1];
+		if (!params_are_valid(str))
 			write(1, "Error\n", 7);
-		else {
-			check_special_cases(array);
-			i = 0;
-			while (i <= SIZE + 1)
-			{
-				j = 0;
-				while (j <= SIZE + 1)
-				{
-					printf("%d ", array[i][j]);
-					j++;
-				}
-				printf("\n");
-				i++;
+		else
+		{
+			array = generate_array(str);
+			if (!array_is_valid(array))
+				write(1, "Error\n", 7);
+			else {
+				check_special_cases(array);
+				print_array(array);
 			}
 		}
 	}
